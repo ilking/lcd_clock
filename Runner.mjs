61992731909e1062de01584d00146e5025c923ae
ianking <king.ian.ilk@gmail.com>
2021-07-08 09:24:28 -0400

    extend on time

--- Runner.mjs
+++ Runner.mjs
@@ -6,7 +6,7 @@ import AudioManager from './AudioManager.mjs';
 import FedHolidayWrapper from './FedHolidayWrapper.mjs';
 
 const WAKE_UP_TIME = '5 7 * * 1-5';
-const RESET_TIME = '25 7 * * *';
+const RESET_TIME = '0 9 * * *';
 
 const FourthLineMode = {
   ALARM: 0,
