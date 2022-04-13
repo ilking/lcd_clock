3dd40171ee4b38771a202e4cc875a80c1d4e3893
ianking <king.ian.ilk@gmail.com>
2022-04-13 17:48:27 -0400

    Updates

--- Runner.mjs
+++ Runner.mjs
@@ -5,8 +5,8 @@ import cron from 'node-cron';
 import AudioManager from './AudioManager.mjs';
 import FedHolidayWrapper from './FedHolidayWrapper.mjs';
 
-const WAKE_UP_TIME = '35 6 * * 1-5';
-const RESET_TIME = '45 6 * * *';
+const WAKE_UP_TIME = '10 7 * * 1-5';
+const RESET_TIME = '15 7 * * *';
 const SCREEN_OFF_TIME = '5 7 * * *';
 
 const FourthLineMode = {
@@ -78,9 +78,23 @@ export default class Runner {
     cron.schedule(WAKE_UP_TIME, () => this.runWakeUpLine());
     cron.schedule(RESET_TIME, () => this.releaseFourthLine());
     cron.schedule(SCREEN_OFF_TIME, () => this.screenOff());
-
+    
     setInterval(() => {
       this.writeStaticLines();
     }, 1000);
+  }
+
+  run_test() {
+    // this.screenOff();
+    // cron.schedule(WAKE_UP_TIME, () => this.runWakeUpLine());
+    // cron.schedule(RESET_TIME, () => this.releaseFourthLine());
+    // cron.schedule(SCREEN_OFF_TIME, () => this.screenOff());
+    this.lcd.displayOn();
+    // this.runWakeUpLine();
+
+    this.writeStaticLines();
+    setInterval(() => {
+      this.writeStaticLines();
+    }, 3000);
   }
 }
